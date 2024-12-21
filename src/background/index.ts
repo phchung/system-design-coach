30d31e04b5ac6feef00f3672d54217fa1d747f01
phchung <phillipchung@c.local>
2024-12-21 02:34:40 -0800

    added npm watch

--- src/background/index.ts
+++ src/background/index.ts
@@ -8,7 +8,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       // call chatgpt api here and send message
       console.log("HEREEEE")
       console.log(message)
-      console.log(sender)
       const response =
         'You can reduce the interval duration (20 milliseconds) to a smaller value to make the typewriter effect print faster. For example, you can try reducing it to 10 milliseconds or even smaller values like 5 milliseconds. However, keep in mind that reducing the interval duration too much may impact performance, so its essential to find a balance between speed and performance.'
       // @ts-expect-error
